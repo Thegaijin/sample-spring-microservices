9e0a28f832116ccb136039956fc59474c728d3bb
Thegaijin <ssemagandavictor@gmail.com>
2018-08-23 02:01:57 +0300

    removed namespace command

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 kubectl apply -R -f ./k8s/customer
 kubectl apply -R -f ./k8s/account
-kubectl create namespace ingress
 kubectl apply -R -f ./k8s/default-backend
 kubectl apply -f ./k8s/nginx/nginx-ingress-controller-config-map.yaml -n=ingress
 kubectl apply -f ./k8s/nginx/nginx-ingress-controller-roles.yaml -n=ingress
