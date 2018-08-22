64d84d2423bcf5eb77baa5a491f9601e90da73cc
Thegaijin <ssemagandavictor@gmail.com>
2018-08-23 01:48:53 +0300

    find deploy sdfgnf

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-kubectl create -R -f ./customer-service
-kubectl create -R -f ./account-service
+kubectl apply -R -f ./customer-service
+kubectl apply-R -f ./account-service
 kubectl create namespace ingress
-kubectl create -R -f ./default-backend
-kubectl create -f ./nginx/nginx-ingress-controller-config-map.yaml -n=ingress
-kubectl create -f ./nginx/nginx-ingress-controller-roles.yaml -n=ingress
-kubectl create -f ./nginx/nginx-ingress-controller-deployment.yaml -n=ingress
-kubectl create -f ./nginx/nginx-ingress.yaml -n=ingress
-kubectl create -f app-ingress.yaml
-kubectl create -f ./nginx/nginx-ingress-controller-service.yaml -n=ingress
+kubectl apply -R -f ./default-backend
+kubectl apply -f ./nginx/nginx-ingress-controller-config-map.yaml -n=ingress
+kubectl apply -f ./nginx/nginx-ingress-controller-roles.yaml -n=ingress
+kubectl apply -f ./nginx/nginx-ingress-controller-deployment.yaml -n=ingress
+kubectl apply -f ./nginx/nginx-ingress.yaml -n=ingress
+kubectl apply -f app-ingress.yaml
+kubectl apply -f ./nginx/nginx-ingress-controller-service.yaml -n=ingress
